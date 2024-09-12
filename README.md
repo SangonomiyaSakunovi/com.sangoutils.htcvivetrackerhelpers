f16f3fb7d79d9c80fe7a7d5c32611bb3594b3eb0
Sangonomiya Sakunovi <1602274566@qq.com>
2024-09-12 09:39:03 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# com.sangoutils.htcvivetrackerhelpers
+ If you use HTC VIVE Tracker, you can use this to help you, and contains the WebCameraHelper
---
+++ Runtime/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("com.sangoutils.htcvivetrackerhelper.editor")]
+namespace SangoUtils.HTCViveTrackerHelpers
+{
+    internal class AssemblyInfo
+    {
+    }
+}
